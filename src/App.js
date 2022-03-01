d884f3652e2014a3a0e405dd5eaaf7320c51ddfd
Bundy-Mundi <bennie.ben095@gmail.com>
2022-02-28 21:22:50 -0800

    Add redux signout user to authStateChagned

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 // Redux
 import { useSelector, useDispatch } from 'react-redux';
-import { signInFoodTruck, signInUser } from "./redux/slices/userSlice";
+import { signInFoodTruck, signInUser, signOutUser } from "./redux/slices/userSlice";
 // import { setError } from "./redux/slices/errorSlice";
 import { setLoading, endLoading } from "./redux/slices/loadingSlice";
 
@@ -34,7 +34,7 @@ function App() {
     }
   }, [user]);
 
-  
+  // Authflag needed because onAuthStateChanged get triggered twice
   onAuthStateChanged(auth, async(user) => {
     if(authFlag){
       authFlag = false;
@@ -46,9 +46,10 @@ function App() {
           dispatch(signInUser({uid:user.uid, email: user.email}));
         dispatch(endLoading());
       } else if (user == null) {
+        dispatch(signOutUser());
         dispatch(endLoading());
       }
-    }
+    } // authFlag ends
   });
 
   // Loading component
