48c28aac54699e6a05c0a2c5f31cfd427bbb6039
Bundy-Mundi <bennie.ben095@gmail.com>
2022-03-29 12:37:31 -0700

    Fixed Icon style

--- src/pages/Map/Geolocator/FoodTruckMarker.jsx
+++ src/pages/Map/Geolocator/FoodTruckMarker.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 import { Truck } from "@styled-icons/fa-solid";
+import { CloudOffline } from "@styled-icons/fluentui-system-regular"
+
 const FoodTruckMarker = ({lat, lng, text, online, myFoodTruck}) => {
     let color = "gray-800";
-    let size = "h-5 w-5";
-    if (myFoodTruck) size = "h-6 w-6"; 
-    if (online) color = "red-500";
+    if (online) color = "green-500";
     return (
-        <div className={`text-${color} ${size} font-bold`}>
-            <Truck/>
-            <div>
-                <p>{text}</p>
+        <div className={`text-${color} w-12 h-12 font-bold grid grid-cols-5 grid-rows-4`}>
+            <div className="row-span-3 col-span-3 flex items-center justify-center ">
+                <Truck size={30}/>
+            </div>
+            <div className="row-span-3 col-span-2 flex items-start justify-end text-black">
+                {
+                !online ?
+                <CloudOffline size={30}/>
+                    :
+                null
+                }
+            </div>
+            <div className="row-span-1">
+                <p style={{fontSize: ".6rem"}} className="text-gray-800">{text}</p>
             </div>
         </div>
     )
